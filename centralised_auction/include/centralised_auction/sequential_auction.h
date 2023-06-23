b73b5d4fa74d593b04c898d9722cb061fe21c4e1
Nicu Stiurca <nicu@traclabs.com>
2023-06-23 17:38:58 -0500

    Add getter for the allocated paths by index

--- centralised_auction/include/centralised_auction/sequential_auction.h
+++ centralised_auction/include/centralised_auction/sequential_auction.h
@@ -8,51 +8,52 @@
 #include "task_msgs/TaskArray.h"
 #include "geometry_msgs/Pose.h"
 
+using geometry_msgs::Pose;
 using std::cout;
 using std::endl;
 using std::vector;
 using task_msgs::Task;
 using task_msgs::TaskArray;
-using geometry_msgs::Pose;
 
+class SequentialAuction
+{
+public:
+  bool use_least_contested_bid;
+  bool return_home;
 
-class SequentialAuction {
-  public:
-    bool              use_least_contested_bid;
-    bool              return_home;
-    
-    SequentialAuction(vector<Task> unallocated_tasks,
-                      vector<Pose> robot_poses);
-    vector<TaskArray> allocateTasks();
-    
-  private:
-    // These variables are unchanged through allocation.
-    int               num_robots;
-    int               num_tasks;
-    vector<Pose>      robot_poses;
-    vector<Task>      tasks;
-    // Working variables for alloction.
-    vector<int>              unalloc;
-    vector< vector<int> >    paths;       //consider changing to lists for efficiency
-    vector<double>           path_costs;
-    vector< vector<double> > bids;
-    
-    // Output variable from allocation.
-    vector<TaskArray> allocations;
-    
-    void formOutput();
-    void processWinner(int winning_robot, int winning_task);
-    void selectWinner(int &winning_robot, int &winning_task);
-    void prepareAllocations();
-    void calculateAllBids();
-    void calculateBids(int robot_num);
-    double insertTask( int robot_num, int unalloc_id, vector<int>& new_path);
-    double calculatePathCost( int robot_num, vector<int> path );
-    void printPaths();
-    void printPath( vector<int> path );
-    void printBids();
+  SequentialAuction(vector<Task> unallocated_tasks, vector<Pose> robot_poses);
+  vector<TaskArray> allocateTasks();
+  vector<vector<int> > getPaths() const
+  {
+    return paths;
+  }
 
+private:
+  // These variables are unchanged through allocation.
+  int num_robots;
+  int num_tasks;
+  vector<Pose> robot_poses;
+  vector<Task> tasks;
+  // Working variables for alloction.
+  vector<int> unalloc;
+  vector<vector<int> > paths;  // consider changing to lists for efficiency
+  vector<double> path_costs;
+  vector<vector<double> > bids;
+
+  // Output variable from allocation.
+  vector<TaskArray> allocations;
+
+  void formOutput();
+  void processWinner(int winning_robot, int winning_task);
+  void selectWinner(int& winning_robot, int& winning_task);
+  void prepareAllocations();
+  void calculateAllBids();
+  void calculateBids(int robot_num);
+  double insertTask(int robot_num, int unalloc_id, vector<int>& new_path);
+  double calculatePathCost(int robot_num, vector<int> path);
+  void printPaths();
+  void printPath(vector<int> path);
+  void printBids();
 };
 
-
-#endif //SEQUENTIAL_AUCTION_H
+#endif  // SEQUENTIAL_AUCTION_H
